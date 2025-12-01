8278a39ca39708c729711ae42a0471a82692c0d8
TheGenFire <168192360+TheGenFire@users.noreply.github.com>
2025-12-01 09:00:17 -0800

    Add files via upload

---
+++ Source Code/Login/drainage-system_database.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE drainage_system;
+
+USE drainage_system;
+
+CREATE TABLE roles (
+    roleID INT AUTO_INCREMENT PRIMARY KEY,
+    roleName VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE users (
+    userID INT AUTO_INCREMENT PRIMARY KEY,
+    roleID INT NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    firstName VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    contactNumber VARCHAR(20) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    dateRegistered DATE NOT NULL,
+    FOREIGN KEY (roleID) REFERENCES roles(roleID)
+	ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+
+CREATE TABLE REPORTS (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  userId INT,
+  reportType VARCHAR(100),
+  description TEXT,
+  status VARCHAR(50),
+  severity VARCHAR(50),
+  location VARCHAR(255),
+  latitude DECIMAL(10,6),
+  longitude DECIMAL(10,6),
+  dateFiled DATE,
+  dateResolved DATE NULL,
+  FOREIGN KEY (userId) REFERENCES users(id)
+);
+
+DROP TABLE reports;
+
+INSERT INTO users (userName, email, password, role) 
+VALUES ('Kier Borne', 'bornekier@gmail.com', MD5('12345'), 'Resident');
+
+
+
+
+SELECT email, password FROM USERS WHERE email = 'bornekier@gmail.com';
+
+
+
+SELECT * from REPORTS;
